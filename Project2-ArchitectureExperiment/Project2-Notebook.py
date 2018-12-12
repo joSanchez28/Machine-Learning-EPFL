af1cebe7daf2f0d4b18c0a7ad7cbca70c0527216
Carlos <charli557@gmail.com>
2018-12-13 00:50:11 +0100

    Ridge regression and Minimal changes in preprocessing

--- Project2-ArchitectureExperiment/Project2-Notebook.py
+++ Project2-ArchitectureExperiment/Project2-Notebook.py
@@ -12,7 +12,7 @@
 # - seaborn
 # - keras
 
-# In[3]:
+# In[4]:
 
 
 import pandas as pd
@@ -27,7 +27,7 @@ import seaborn as sns
 get_ipython().magic(u'matplotlib inline')
 
 
-# In[4]:
+# In[5]:
 
 
 DATA_FOLDER = './data/' #The data is in the .gitignore in order to not upload it to the GitHub repository
@@ -35,10 +35,19 @@ DATA_FOLDER = './data/' #The data is in the .gitignore in order to not upload it
 
 # Execute the following line to export from the jupyter notebook(.ipynb) to a .py file (ignore the warnings):
 
-# In[5]:
+# In[6]:
 
 
 #!jupyter nbconvert --to script Project2-Notebook.ipynb 
+
+
+# For not show the warnings:
+
+# In[7]:
+
+
+import warnings
+warnings.filterwarnings(action = 'once')
 
 
 # ## Exploratory Data Analysis
@@ -48,7 +57,7 @@ DATA_FOLDER = './data/' #The data is in the .gitignore in order to not upload it
 # First we load the data into a Pandas data frame (with the Pandas library; to install this package with conda run:
 # `conda install -c anaconda pandas`):
 
-# In[6]:
+# In[8]:
 
 
 df_interior_features = pd.read_csv(DATA_FOLDER + 'T_features.csv')
@@ -57,19 +66,19 @@ df_labels_and_features = pd.read_csv(DATA_FOLDER + 'table_dataset_GreeceSwitzerl
 
 # Let's see what is inside `table_dataset_GreeceSwitzerland_N265_metrics_mSC5_JPEGtoBMP_Michelson_RMS.csv`:
 
-# In[7]:
+# In[9]:
 
 
 df_labels_and_features.head(7)
 
 
-# In[8]:
+# In[10]:
 
 
 df_labels_and_features.shape
 
 
-# In[9]:
+# In[11]:
 
 
 df_labels_and_features.columns
@@ -89,19 +98,19 @@ df_labels_and_features.columns
 
 # Now let's see what is inside `T_features.csv`:
 
-# In[10]:
+# In[12]:
 
 
 df_interior_features.head()
 
 
-# In[11]:
+# In[13]:
 
 
 df_interior_features.shape
 
 
-# In[12]:
+# In[14]:
 
 
 df_interior_features.columns
@@ -123,13 +132,19 @@ df_interior_features.columns
 # 
 # Secondly, the labels (outputs) of our new data frame are the data that comes from every survey (which has been done by each person): 'pleasant', 'interesting', 'exciting', 'calming', 'complex', 'bright', 'view', 'spacious'. We are asked to study first the 'exciting' and the 'calming' labels but we will split the data frame afterwards.
 
-# In[13]:
+# In[15]:
 
 
-df_labels_and_features.head(1)
+df_labels_and_features.sample()
 
 
-# In[14]:
+# In[16]:
+
+
+df_labels_and_features[df_labels_and_features['ID']==21]
+
+
+# In[17]:
 
 
 df_interior_features.head(1)
@@ -137,7 +152,7 @@ df_interior_features.head(1)
 
 # For doing the join, we check that one of the metrics determine totally the interior (there are not two interiors with the same metric value):
 
-# In[15]:
+# In[18]:
 
 
 df_interior_features["to_join"]=(1000*df_interior_features["contrast_mean_RMS"]).astype(int) #We cannot do the join on the float
@@ -146,34 +161,48 @@ df_labels_and_features["to_join"]=(1000*df_labels_and_features["contrast_mean_RM
 df_interior_features.set_index("to_join").index.is_unique
 
 
-# In[16]:
+# In[19]:
 
 
-df_ml = df_interior_features.merge(df_labels_and_features, on = "to_join", how = 'inner', suffixes = ("_a",""))
-df_ml.head(1)
+df_labels_and_features.columns
+
+
+# In[20]:
+
+
+df_ml_raw = df_interior_features.merge(df_labels_and_features, on = "to_join", how = 'inner', suffixes = ("_a",""))
+df_ml_raw.head(1)
+
+
+# In[21]:
+
+
+df_ml_raw.columns
 
 
 # Now we drop the duplicates columns and order the columns in order to have a data frame with the structure X|Y, where X is the matrix of features (each column is a feature) and Y is the matrix with the labels.
 
-# In[17]:
+# In[22]:
 
 
-df_ml = df_ml[['Country', 'Gender', 'Pattern', 'Context', 'SkyType', 'contrast_mean_mSC5', 'contrast_max_mSC5', 
+df_ml = df_ml_raw[['ID', 'Country', 'Gender', 'Pattern', 'Context', 'SkyType', 'contrast_mean_mSC5', 'contrast_max_mSC5', 
                'contrast_mean_Michelson', 'contrast_mean_Michelson_cube123', 'contrast_mean_Michelson_cube3', 
                'contrast_mean_RMS', 'contrast_mean_RMS_cube123', 'contrast_mean_RMS_cube3', 'complexity_mean_JPEGtoBMP',
                'complexity_cube3_JPEGtoBMP', 'complexity_cube123_JPEGtoBMP', 'pleasant', 'interesting', 'exciting', 'calming', 
                'complex', 'bright', 'view', 'spacious']]
 
 
-# In[18]:
 
 
-df_ml.head(2)
+# In[23]:
+
+
+df_ml.head(1)
 
 
 # We can also check and see that there are no missing values. (all instances are non-null)
 
-# In[19]:
+# In[24]:
 
 
 df_ml.info()
@@ -181,7 +210,7 @@ df_ml.info()
 
 # and see that all ratings are between 0 and 10:
 
-# In[20]:
+# In[25]:
 
 
 df_ml[['pleasant', 'interesting', 'exciting', 'calming', 'complex', 'bright',
@@ -190,14 +219,14 @@ df_ml[['pleasant', 'interesting', 'exciting', 'calming', 'complex', 'bright',
 
 # Finally, from this data frame is easy to get X and the labels desired (y) separately. So we get the data frame which contains the matrix of features X and, separately,the labels corresponding to 'exciting' and 'calming' (we were asked to analize this labels firstly):
 
-# In[21]:
+# In[26]:
 
 
 x_fdata = df_ml.iloc[:,0:16] #fdata -> first data without preprocessing
 x_fdata.head(5)
 
 
-# In[22]:
+# In[27]:
 
 
 y_data = df_ml["exciting"]
@@ -215,7 +244,7 @@ y_data.to_frame().head() #Note that y_data is a Pandas serie
 
 # We can confirm this by checking the type of data for each feature:
 
-# In[23]:
+# In[28]:
 
 
 x_fdata.dtypes
@@ -225,7 +254,7 @@ x_fdata.dtypes
 # 
 # Let's inspect these features closely:
 
-# In[24]:
+# In[29]:
 
 
 categorical_features = ['Country','Gender','Pattern','Context',                         'SkyType']
@@ -236,7 +265,7 @@ for feat in categorical_features:
 
 # In order to perform dummy variable encoding on the categorical features, we can use the pandas method `pd.get_dummies()`. Also since we need k-1 dummy variables to represent k categories, we can drop the first column for each encoding (`drop_first = True`). We'll store this as a new dataframe `dummy_df`.
 
-# In[25]:
+# In[30]:
 
 
 dummy_df = pd.get_dummies(x_fdata, columns=categorical_features, drop_first=False)
@@ -245,7 +274,7 @@ dummy_df.head(3)
 
 # We standardize the features in preparation for the training (output excluded, "exciting" column).
 
-# In[26]:
+# In[31]:
 
 
 x_without_std = dummy_df.copy() #x_without_std is the final standardized data (Just in case you want to use it)
@@ -255,7 +284,7 @@ for feat in dummy_df.columns.tolist():
     dummy_df[feat] = (dummy_df[feat] - mean)/std
 
 
-# In[27]:
+# In[32]:
 
 
 x_data = dummy_df #x_data will be the final standardized data
@@ -267,7 +296,7 @@ x_data = dummy_df #x_data will be the final standardized data
 
 # First let's plot the distribution of exciting
 
-# In[28]:
+# In[60]:
 
 
 print(y_data.describe())
@@ -279,7 +308,7 @@ sns.distplot(y_data, color='g');
 
 # Now we check the correlation between the **numeric** features and 'exciting'
 
-# In[29]:
+# In[27]:
 
 
 numeric_data = pd.concat([y_data, x_data], axis=1, sort=False).drop( [ 'Country_Greece', 'Country_Switzerland',
@@ -303,7 +332,7 @@ numeric_data.corr()['exciting']
 
 # We visualize the relations between "exciting"(response variable) and the features
 
-# In[28]:
+# In[62]:
 
 
 for feature in numeric_data.columns:
@@ -315,7 +344,7 @@ for feature in numeric_data.columns:
 
 # #### hex plot
 
-# In[29]:
+# In[61]:
 
 
 for feature in numeric_data.columns:
@@ -324,7 +353,7 @@ for feature in numeric_data.columns:
 
 # #### kernel density plot
 
-# In[30]:
+# In[63]:
 
 
 for feature in numeric_data.columns:
@@ -336,7 +365,7 @@ for feature in numeric_data.columns:
 # 
 # we can also demonstrate the linear regression line estimate for each feature.
 
-# In[114]:
+# In[64]:
 
 
 fig, ax = plt.subplots(round(len(numeric_data.columns) / 3), 3, figsize = (18, 12))
@@ -347,7 +376,7 @@ for i, ax in enumerate(fig.axes):
 
 # Now lets plot box plots for categorical features to see the relashionship between them and exciting
 
-# In[115]:
+# In[32]:
 
 
 # we need the categorical variables in their originaL(non dummy) form
@@ -355,7 +384,7 @@ categ_data = x_fdata[categorical_features]
 
 
 
-# In[116]:
+# In[33]:
 
 
 fig, ax = plt.subplots(round(len(categ_data.columns) / 3), 3, figsize = (18, 12))
@@ -367,7 +396,7 @@ for i, ax in enumerate(fig.axes):
 
 # we can also look at the distributions of categorical variabels
 
-# In[117]:
+# In[34]:
 
 
 fig, axes = plt.subplots(round(len(categ_data.columns) / 3), 3, figsize=(12, 5))
@@ -383,7 +412,7 @@ fig.tight_layout()
 
 # Now lets take a look at the correlation between the **numeric** features
 
-# In[118]:
+# In[35]:
 
 
 # remove the output variable
@@ -404,7 +433,7 @@ sns.heatmap(corr[(corr >= 0.7) | (corr <= -0.7)],
 
 # In order to split the data into the two sets (training and test), first we need to rejoin the y_data and the x_data. Then we can use the sample method (`pd.sample()`) which  randomly samples from the dataframe according to a given ratio (0.8 for train in this case). The remaining part of the data will be the test set.
 
-# In[119]:
+# In[36]:
 
 
 #In order to split the data, we join then again:
@@ -412,20 +441,20 @@ data = pd.concat([y_data, x_data], axis=1, sort=False)
 data.head(1)
 
 
-# In[120]:
+# In[37]:
 
 
 train_df = data.sample(frac=0.8,replace=False)
 test_df = data.drop(train_df.index.tolist(),axis=0)
 
 
-# In[121]:
+# In[38]:
 
 
 train_df.head(3)
 
 
-# In[122]:
+# In[39]:
 
 
 test_df.head(3)
@@ -433,7 +462,7 @@ test_df.head(3)
 
 # Quick check of the validity of the split by making sure that the size of the train plus the size of the test equals the size of the dummy:
 
-# In[123]:
+# In[40]:
 
 
 print(train_df.shape[0] + test_df.shape[0] == dummy_df.shape[0])
@@ -441,7 +470,7 @@ print(train_df.shape[0] + test_df.shape[0] == dummy_df.shape[0])
 
 # Double check the mean of the features
 
-# In[124]:
+# In[41]:
 
 
 #dummy_df.mean()
@@ -451,7 +480,7 @@ print(train_df.shape[0] + test_df.shape[0] == dummy_df.shape[0])
 
 # Now for the standart deviation
 
-# In[125]:
+# In[42]:
 
 
 #dummy_df.std()
@@ -459,7 +488,7 @@ print(train_df.shape[0] + test_df.shape[0] == dummy_df.shape[0])
 
 # Hence our standardized features and our labels are ready to go.
 
-# In[ ]:
+# In[43]:
 
 
 x_train = train_df.iloc[1:-1]
@@ -475,26 +504,26 @@ y_test = train_df.iloc[0:1]
 
 # ## Linear Regression 
 
-# In[32]:
+# In[44]:
 
 
 from sklearn import linear_model
 
 
-# In[33]:
+# In[45]:
 
 
 reg = LinearRegression()
 reg.fit(x_data, y_data)
 
 
-# In[34]:
+# In[46]:
 
 
 reg.coef_
 
 
-# In[35]:
+# In[47]:
 
 
 reg.score(x_data, y_data)
@@ -504,7 +533,7 @@ reg.score(x_data, y_data)
 # 
 # Lets also calculate accuracy since our problem can also be seen as classification
 
-# In[36]:
+# In[48]:
 
 
 def compute_accuracy(model, x_data, y_true):
@@ -524,13 +553,13 @@ print(compute_accuracy(reg, x_data, y_data))
 
 # This is a very low accuracy. Let's try polynomial features as well.
 
-# In[37]:
+# In[49]:
 
 
 from sklearn.preprocessing import PolynomialFeatures
 
 
-# In[38]:
+# In[50]:
 
 
 for i in range(1,5):
@@ -548,9 +577,193 @@ for i in range(1,5):
     print("score = %f \n" %(reg.score(poly_x, y_data)) )
 
 
+# as we can see the accuracy is not satesfactory 
+
+# ## Matrix Factorization and recommender systems
+
+# First let's make the ratings matrix ratings. in this matrix each row represents a person and each coluimn a type of room. The cells are filled with the value of exciting (the rating people gave the room).
+
+# We go back and use our raw data frame to include the ID column as well.
+# In this way ID and Country combined give us a uniqe index for each person.
+
+# In[33]:
+
+
+df_people = df_ml_raw[['ID', 'Country', 'Pattern', 'Context', 'SkyType', 'exciting']]
+
+
+# In[36]:
+
+
+df_people[df_people['ID']==3]
+
+
+# Now we make a pivot table out of the data. each row identified by 'ID' and 'Country' and each columnn identified by the room specifications ('Pattern', 'Context', 'SkyType'). 
+
+# In[3]:
+
+
+#df_pivot_table = pd.pivot_table(df_people, values='exciting', index=['ID', 'Country'], columns=['Pattern', 'Context', 'SkyType'], aggfunc=np.mean)
+
+
+# In[2]:
+
+
+#df_pivot_table.sample(4)
+
+
+# We are going to use the surprise library for recommender systems.
+# 
+# (can be downloaded from here: https://surprise.readthedocs.io/en/stable/index.html)
+
+# In[1]:
+
+
+from surprise import NormalPredictor
+from surprise import Dataset
+from surprise import Reader
+from surprise.model_selection import cross_validate
+from surprise import NMF
+
+
+# To load data into the format of he library:
+# The columns of the data frame must correspond to user id, item id and ratings (in that order). 
+# 
+# First we concatenate the ID and Country to get the final uniqe ID of each person.
+
+# In[57]:
+
+
+# cast the integer ID columnn to string as well
+people_ID = df_people['ID'].astype(str).str.cat(df_people['Country'], sep='_')
+
+#people_ID[people_ID == '11_Greece']
+
+
+
+# We also concatenate pattern, context and skytype to get the IDs for rooms
+
+# In[58]:
+
+
+room_ID = df_people['Pattern'].str.cat(df_people['Context'], sep='_').str.cat(df_people['SkyType'], sep='_')
+
+#room_ID[room_ID=='P1EL_social_clearhigh']
+
+
+# In[59]:
+
+
+final_df =pd.DataFrame(df_people.exciting)
+
+final_df['people_ID'] = people_ID
+final_df['room_ID'] = room_ID
+
+#The columns of the data frame must correspond to user id, item id and ratings (in that order). 
+final_df = final_df[['people_ID', 'room_ID', 'exciting']]
+
+
+# In[60]:
+
+
+final_df.head()
+
+
+# Now we are ready to Use the library. 
+# First we tell the reader the scale of the rating.
+# Then we load the data.
+
+# In[62]:
+
+
+reader = Reader(rating_scale=(0, 10))
+
+surprise_data = Dataset.load_from_df(final_df, reader)
+
+
+# Now we use the Non negative matrix factorization algorithm.
+
+# We can tune the parameters using the GridSearchCV.
+
+# In[80]:
+
+
+from surprise.model_selection import GridSearchCV
+
+param_grid = {'n_factors':[15, 20, 30, 40, 50], 'n_epochs': [50, 100]}
+
+algo = NMF
+
+gs = GridSearchCV(algo, param_grid, measures=['rmse', 'mae'], cv=3)
+
+gs.fit(surprise_data)
+
+# best RMSE score
+print(gs.best_score['rmse'])
+
+
+
+# combination of parameters that gave the best RMSE score
+print(gs.best_params['rmse'])
+
+
+
+# In[78]:
+
+
+pd.DataFrame(gs.cv_results)[['mean_test_rmse', 'param_n_factors', 'param_n_epochs' ]]
+
+
+# Now we use the features we have caculated using Matrix factorization and the features we previously had to perform a linear regression on the data.
+
+# First we train on the whole data set. 
+
+# In[83]:
+
+
+# Retrieve the trainset.
+trainset = surprise_data.build_full_trainset()
+
+# Build an algorithm, and train it.
+algo = NMF(n_factors=50, n_epochs=100)
+algo.fit(trainset)
+
+
+# Now we have all the features in the algo.qi.
+# for each type of room we have 50 features.
+# 
+
+# In[100]:
+
+
+room_features = pd.DataFrame(algo.qi)
+room_features.shape
+
+
+# Concatenate the ids of each room to it's row.
+
+# In[111]:
+
+
+r_id = []
+for i in range(0,36):
+    r_id.append(trainset.to_raw_iid(i))
+room_features['r_id'] = r_id
+room_features.head(2)
+
+
+# We will join the data frame df_people based on room_ID and people_ID to get the final table.
+
+# In[113]:
+
+
+#temp = df_people.merge(room_features, on = "r_id", how = 'inner', suffixes = ("_a",""))
+#temp.head(1)
+
+
 # ## Random Forest
 
-# In[39]:
+# In[65]:
 
 
 from sklearn.ensemble import RandomForestClassifier
@@ -560,14 +773,14 @@ from sklearn.ensemble import RandomForestClassifier
 
 # Firstly, we do grid search on the number of estimators "n_estimators" and the max depth of the trees "max_depth". We decide to use the accuracy as a criteria for comparing the quality of the diferents models (created by differents "n_estimators" and "max_depth"). So, we compute a data frame with the accuracy for every pair (n_estimators, max_depth) explorated.
 
-# In[46]:
+# In[52]:
 
 
 n_estimators_range=range(1,100, 5)
 max_depth_range= range(1,100, 5)
 
 
-# In[ ]:
+# In[53]:
 
 
 performance_results = pd.DataFrame(columns=['n_estimators', 'max_depth','accuracy', 'mean_error_scorer'])
@@ -585,13 +798,13 @@ for i in n_estimators_range:
 performance_results.head()
 
 
-# In[ ]:
+# In[54]:
 
 
 performance_results.sort_values(by = "accuracy", ascending=False).head(2)
 
 
-# In[ ]:
+# In[55]:
 
 
 performance_results.sort_values(by = "mean_error_scorer", ascending=False).head(3)
@@ -599,7 +812,7 @@ performance_results.sort_values(by = "mean_error_scorer", ascending=False).head(
 
 # ## Decision Trees
 
-# In[100]:
+# In[56]:
 
 
 from sklearn import tree
@@ -629,7 +842,7 @@ print(mse_DTR_cross)
 
 # ## DENSE NEURAL NETWORKS
 
-# In[62]:
+# In[57]:
 
 
 # Use conda install -c conda-forge keras; conda install -c anaconda keras
